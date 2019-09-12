4b2a784637d72db9c0a63b7c6f7eec4ec8061f85
Adeepa <akalankaadeepa@gmail.com>
2019-09-12 23:46:02 +0530

    adeepa testtttttt

---
+++ app/src/main/java/com/example/eventhub/AddPackage2.java
@@ -0,0 +1,14 @@
+package com.example.eventhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPackage2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_package2);
+    }
+}
