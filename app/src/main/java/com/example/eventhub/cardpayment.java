e15ebcbb827be768be69912113d7cd0b3218cfb1
DELL <kavinduj511@gmail.com>
2019-09-26 14:35:44 +0530

    kavindu cruds

--- app/src/main/java/com/example/eventhub/cardpayment.java
+++ app/src/main/java/com/example/eventhub/cardpayment.java
@@ -3,19 +3,121 @@ package com.example.eventhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.text.TextUtils.isEmpty;
+
 public class cardpayment extends AppCompatActivity {
+
+    Button paysub;
+    EditText payname, paynum, paycvv, payexd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardpayment);
+
+
+        payname = findViewById(R.id.payxx);
+        paynum = findViewById(R.id.paynum);
+        paycvv = findViewById(R.id.paycvv);
+        payexd = findViewById(R.id.payzz);
+
+
+        paysub = (Button)findViewById(R.id.paysub);
+
+
+        paysub.setOnClickListener(new View.OnClickListener() {
+
+            payy x = new payy();
+
+            //@Override
+            public void onClick(View view) {
+                //AddEvent2 addEvent2 = new AddEvent2();
+                // AddEvent2 addEvent2 = new AddEvent2();
+
+                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("payment");
+                try {
+                    if (isEmpty(payname.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter event ID", Toast.LENGTH_LONG).show();
+                    else if (isEmpty((paynum.getText().toString())))
+                        Toast.makeText(getApplicationContext(), "Please enter event name", Toast.LENGTH_LONG).show();
+                    else if (isEmpty((paycvv.getText().toString())))
+                        Toast.makeText(getApplicationContext(), "Please enter event category", Toast.LENGTH_LONG).show();
+                    else if (isEmpty((payexd.getText().toString())))
+                        Toast.makeText(getApplicationContext(), "Please enter event venue", Toast.LENGTH_LONG).show();
+                /*else if (isEmpty((txtcontact.getText().toString())))
+                    Toast.makeText(getApplicationContext(), "Please enter your contact number", Toast.LENGTH_LONG).show();
+                else if (isEmpty((txtcheckin.getText().toString())))
+                    Toast.makeText(getApplicationContext(), "Please enter your Check-In date", Toast.LENGTH_LONG).show();
+                else if (isEmpty((txtcheckout.getText().toString())))
+                    Toast.makeText(getApplicationContext(), "Please enter your Check-Out date", Toast.LENGTH_LONG).show();*/
+                    else {
+
+
+                        x.setPayname(payname.getText().toString().trim());
+                        x.setPaynum(paynum.getText().toString().trim());
+                        x.setPaycvv(paycvv.getText().toString().trim());
+                        x.setPayexd(payexd.getText().toString().trim());
+                    /*addEvent2.setCheckout(txtcheckout.getText().toString().trim());
+                    addEvent2.setFullName(txtname.getText().toString().trim());
+                    addEvent2.setAddress(txtaddress.getText().toString().trim());
+
+                    try{
+                        addEvent2.setContactnumber(Integer.parseInt(txtcontact.getText().toString().trim()));
+                    }catch (NumberFormatException ex1){
+                        Toast.makeText(getApplicationContext(), "Invalid Contact Number", Toast.LENGTH_LONG).show();
+                    }
+
+                    try{
+                        makereservation.setRooms(Integer.parseInt(txtrooms.getText().toString().trim()));
+                    }catch(NumberFormatException e2){
+                        Toast.makeText(getApplicationContext(), "Invalid Room Type", Toast.LENGTH_LONG).show();
+                    }*/
+
+
+                        dbRef.child(x.getPaynum()).setValue(x);
+
+                        Toast.makeText(getApplicationContext(), "Data Saved Successfully", Toast.LENGTH_LONG).show();
+                        clearControls();
+
+                        //Intent i = new Intent(getApplicationContext(), makereservation4.class);
+                        //startActivity(i);
+                    }
+
+                    //});
+
+                } catch (Exception e1) {
+                    Toast.makeText(getApplicationContext(), "Error" + e1, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            public void clearControls() {
+                payname.setText("");
+                paynum.setText("");
+                paycvv.setText("");
+                payexd.setText("");
+                    /*txtcontact.setText("");
+                    txtnic.setText("");
+                    txtrooms.setText("");*/
+            }
+        });
+
     }
 
+
     public void payment(View view){
-        Intent intent0001 = new Intent(cardpayment.this, updateCard.class);
+        Intent intent0001 = new Intent(cardpayment.this, purchase.class);
         startActivity(intent0001);
     }
+
+
 }
--- app/src/main/java/com/example/eventhub/updateCard.java
+++ app/src/main/java/com/example/eventhub/updateCard.java
@@ -3,8 +3,18 @@ package com.example.eventhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class updateCard extends AppCompatActivity {
 
@@ -12,10 +22,115 @@ public class updateCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_card);
+
+
+        final EditText t1 = findViewById(R.id.payunum);
+        final EditText t2 = findViewById(R.id.payuu);
+        final EditText t3 = findViewById(R.id.payucvv);
+        final EditText t4 = findViewById(R.id.paypp);
+        //final EditText t5 = findViewById(R.id.EdayID);
+
+        Button btnView = findViewById(R.id.payupsearch);
+        Button btnUpdate = findViewById(R.id.payupdate);
+        Button btnDelete = findViewById(R.id.paydel);
+
+        //search Method//
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference reafRef = FirebaseDatabase.getInstance().getReference().child("payment").child(t1.getText().toString());
+                reafRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.hasChildren()) {
+                            //t1.setText(dataSnapshot.child("paynum").getValue().toString());
+                            t2.setText(dataSnapshot.child("payname").getValue().toString());
+                            t3.setText(dataSnapshot.child("paycvv").getValue().toString());
+                            t4.setText(dataSnapshot.child("payexd").getValue().toString());
+                            //t5.setText(dataSnapshot.child("exday").getValue().toString());
+                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "No source to display", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+        //Update Method
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference updateRef = FirebaseDatabase.getInstance().getReference().child("payment");
+                updateRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        payy c = new payy();
+
+                        if(dataSnapshot.hasChild(t1.getText().toString())){
+
+                            c.setPaynum(t1.getText().toString());
+                            c.setPayname(t2.getText().toString());
+                            c.setPaycvv(t3.getText().toString());
+                            c.setPayexd(t4.getText().toString());
+                            //ft.setExday(t5.getText().toString());
+
+                            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("payment").child(t1.getText().toString());
+                            dbRef.setValue(c);
+
+                            Toast.makeText(getApplicationContext(),"Data Update Successfully!",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No sourse to update ",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        //delete
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("payment");
+                delRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(t1.getText().toString())){
+                            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("payment").child(t1.getText().toString());
+                            dbRef.removeValue();
+
+                            Toast.makeText(getApplicationContext(),"Data deleted succesfully!",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No Source to delete!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
 
-    public void purcahse02(View view){
-        Intent intent0001 = new Intent(updateCard.this, purchase.class);
-        startActivity(intent0001);
-    }
+
 }
